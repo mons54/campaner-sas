afd7748feef06813d4774d33a89be251312ffec9
jsimonet <j.mons54@gmail.com>
2016-12-18 16:20:58 +0100

    fix

--- app/app.js
+++ app/app.js
@@ -27,6 +27,7 @@
                 /**
                  * Set the meta tags
                  */
+                $rootScope.shortTitle = $route.current.shortTitle;
                 $rootScope.title = $route.current.title;
                 $rootScope.description = $route.current.description;
 
@@ -52,40 +53,47 @@
         function ($routeProvider, $locationProvider) {
             $routeProvider
             .when('/', {
-                title : "Accueil",
-                description: "Conseils en amélioration et embellissement de l'habitat.",
+                shortTitle: "Accueil",
+                title : "Campaner SAS - Isolation, ventilation, converture, façade",
+                description: "Campaner SAS est une entreprise spécialisée dans le conseil et l'accompagnement des projets d'isolation, de ventilation, de converture et de façade à Toul et en Lorraine.",
                 templateUrl: '/app/home/home.html',
                 controller: 'homeCtrl'
             })
             .when('/isolation', {
-                title : "Isolation",
-                description: "Conseils en amélioration et embellissement de l'habitat.",
+                shortTitle: "Isolation de combles perdus et aménagés",
+                title : "Isolation de combles perdus et aménagés",
+                description: "Campaner SAS entreprise spécialisée en isoltaion de combles perdus et aménagés.",
                 templateUrl: '/app/isolation/isolation.html'
             })
             .when('/assainissement', {
-                title : "Assainissement",
-                description: "Conseils en amélioration et embellissement de l'habitat.",
+                shortTitle: "Assainissement et ventilation",
+                title : "Assainissement et ventilation",
+                description: "Campaner SAS entreprise spécialisée en assainissement et ventilation.",
                 templateUrl: '/app/assainissement/assainissement.html'
             })
             .when('/demoussage', {
-                title : "Démoussage",
-                description: "Conseils en amélioration et embellissement de l'habitat.",
+                shortTitle: "Démoussage et hydrofugation de toiture",
+                title : "Démoussage et entretien de converture",
+                description: "Campaner SAS entreprise spécialisée en démoussage et entretien de converture.",
                 templateUrl: '/app/demoussage/demoussage.html'
             })
             .when('/facades', {
-                title : "Facades",
-                description: "Conseils en amélioration et embellissement de l'habitat.",
+                shortTitle: "Façades",
+                title : "Façades, peinture et crépis",
+                description: "Campaner SAS entreprise spécialisée en façades, peinture et crépis.",
                 templateUrl: '/app/facades/facades.html'
             })
             .when('/contact', {
-                title : "Contact",
-                description: "Conseils en amélioration et embellissement de l'habitat.",
+                shortTitle: "Contactez-nous",
+                title : "Contactez-nous",
+                description: "Contactez-nous afin de pouvoir vous conseiller au plus près de vos réels besoins.",
                 templateUrl: '/app/contact/contact.html',
                 controller: 'contactCtrl'
             })
             .when('/bilan', {
-                title : "bilan",
-                description: "Conseils en amélioration et embellissement de l'habitat.",
+                shortTitle: "Demande d'étude et de bilan gratuits",
+                title : "Demande d'étude et de bilan gratuits",
+                description: "Bénéficiez d'une étude et d'un bilan gratuit sur les performances énergétiques, l'état de votre converture et de l'air ambiant de votre maison.",
                 templateUrl: '/app/bilan/bilan.html'
             })
             .otherwise({
